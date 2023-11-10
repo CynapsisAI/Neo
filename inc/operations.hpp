1170c727b02f683610bb9d788b3732a5f17e7b6e
Ali <alialhamadani72@gmail.com>
2023-11-10 02:55:20 -0500

    Deleted File

---
+++ inc/operations.hpp
@@ -0,0 +1,8 @@
+//
+// Created by alial on 2023-11-10.
+//
+
+#ifndef NEO_OPERATIONS_HPP
+#define NEO_OPERATIONS_HPP
+
+#endif //NEO_OPERATIONS_HPP
---
+++ src/operations.cpp
@@ -0,0 +1,7 @@
+//
+// Created by alial on 2023-11-10.
+//
+#include "operations.hpp"
+
+
+
