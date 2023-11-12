b9b84ca8e98418d2111dfdc9a9531c9cab223989
Robby Sodhi <42954808+Robby-Sodhi@users.noreply.github.com>
2023-11-12 18:09:02 -0500

    implemented shape object with outstream overload. Shape object still w.i.p

--- include/neo.hpp
+++ include/neo.hpp
@@ -1,10 +1,21 @@
 //
 // Created by alial on 2023-11-10.
+// this should be the main library import (hence why it includes anything relevant below)
 //
 
 #ifndef NEO_NEO_HPP
 #define NEO_NEO_HPP
 
-#include "array.hpp"
+#include "./ndarray.hpp"
+#include "./structs.hpp"
 
-#endif //NEO_NEO_HPP
+namespace neo
+{
+    // intended to include all relevant helper functions. For instance like numpy.tanspose(obj) -> neo::transpose(obj)
+
+    template <typename T>
+    void transpose(ndarray<T> ndarray)
+    {
+    }
+}
+#endif // NEO_NEO_HPP
